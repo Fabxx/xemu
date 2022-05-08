b3711a5d7d1f657a4d834c3cf4d42c13efaedcd4
Revix <30447649+revix-0@users.noreply.github.com>
2022-05-09 01:05:10 +0200

    remove double free and pixel variable to avoid double pointing

--- hw/xbox/nv2a/pgraph_texture_replacer.c
+++ hw/xbox/nv2a/pgraph_texture_replacer.c
@@ -3,16 +3,13 @@
 
 void texture_download(TextureBinding *texture, SurfaceBinding *surface)
 {
-    void *pixels = surface->texture;
      char filename[20];
 
     snprintf(filename, sizeof(filename), "%lu.bmp", surface->vram_addr);
    
-    SDL_Surface * surf = SDL_CreateRGBSurfaceFrom(pixels, surface->width, surface->height, 8*4, surface->width*4, 0,0,0,0);
+    SDL_Surface * surf = SDL_CreateRGBSurfaceFrom(surface->texture, surface->width, surface->height, 8*4, surface->width*4, 0,0,0,0);
     SDL_SaveBMP(surf, filename);
     SDL_FreeSurface(surf);
-
-    free(pixels);
     free(surface->texture);
     
 }
