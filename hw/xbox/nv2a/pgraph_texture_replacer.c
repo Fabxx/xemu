8d01ea561081b319f4326aa3a485a11485fee0ee
Revix <30447649+revix-0@users.noreply.github.com>
2022-05-09 10:16:36 +0200

    add base to switch between format colors

--- hw/xbox/nv2a/pgraph_texture_replacer.c
+++ hw/xbox/nv2a/pgraph_texture_replacer.c
@@ -1,15 +1,33 @@
 #include "pgraph_texture_replacer.h"
 
-
+//TODO: Check for other texture formats: ARGB BRGA Ecc FRMO PGRAPH CODE nv097 cases and convert with SDL.
 void texture_download(TextureBinding *texture, SurfaceBinding *surface)
 {
+     SDL_surface * surf;
      char filename[20];
 
     snprintf(filename, sizeof(filename), "%lu.bmp", surface->vram_addr);
    
-   //TODO: Check for other texture formats: ARGB BRGA Ecc FRMO PGRAPH CODE nv097 cases and convert with SDL.
-    SDL_Surface * surf = SDL_CreateRGBSurfaceFrom(surface->texture, surface->width, surface->height, 8*4, surface->width*4, 0,0,0,0);
-    SDL_SaveBMP(surf, filename);
+   
+   switch(surface->shape.color_format) {
+        case NV097_SET_TEXTURE_FORMAT_COLOR_SZ_Y8:
+        surf = SDL_CreateY
+
+
+
+
+
+
+       case NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R8G8B8A8:
+       surf = SDL_CreateRGBSurfaceFrom(surface->texture, surface->width, surface->height, 8*4, surface->width*4, 0,0,0,0);
+       SDL_SaveBMP(surf, filename);
+       break;
+       
+   }
+   
+
+   
+    
     SDL_FreeSurface(surf);
     free(surface->texture);
 
