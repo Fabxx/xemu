959457e50edb4cf92fff2272bbf0f8dcb825fa0f
Revix <30447649+revix-0@users.noreply.github.com>
2022-05-09 11:15:17 +0200

    change from bytes per pixel to color_format as it needs to multiply by the channels retreived (4 or less) instead of bytes per pixel

--- hw/xbox/nv2a/pgraph_texture_replacer.c
+++ hw/xbox/nv2a/pgraph_texture_replacer.c
@@ -5,18 +5,23 @@ void texture_download(TextureBinding *texture, SurfaceBinding *surface)
 {
     SDL_surface * surf;
     char filename[20];
-    
+
     snprintf(filename, sizeof(filename), "%lu.bmp", surface->vram_addr);
     surf = SDL_CreateRGBSurfaceFrom(surface->texture, surface->width, surface->height, 
-                                    surface->shape.color_format*surface->fmt.bytes_per_pixel, surface->width*surface->fmt.bytes_per_pixel, 0,0,0,0);
-    SDL_SaveBMP(surf, filename);                                /*multiplying the color format retreived (aka color channels) multiplied by the 
-                                                                color depth (aka the color combinations, 8 bytes, 4 bytes ecc). Need to use a more generic
-                                                                SDL function to store other formats and not RGB only.*/
-    SDL_FreeSurface(surf);
-    free(surface->texture);
-    surface->texture = NULL;
+                                    surface->shape.color_format*surface->fmt.bytes_per_pixel, surface->width*surface->shape.color_format, 0,0,0,0);
+    SDL_SaveBMP(surf, filename);   
 }
    
+
+   
+    
+    SDL_FreeSurface(surf);
+    free(surface->texture);
+
+    surface->texture = NULL;
+
+    
+}
 
 void texture_upload(TextureBinding *texture, SurfaceBinding *surface)
 {
