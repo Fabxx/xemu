fb0d53385f3335307c7d57f01dc179d6a7e1c3c8
Revix <30447649+revix-0@users.noreply.github.com>
2022-05-09 10:53:29 +0200

    get rid of the switch case and make the retreival of the color channels and byte per channel more generic

--- hw/xbox/nv2a/pgraph_texture_replacer.c
+++ hw/xbox/nv2a/pgraph_texture_replacer.c
@@ -3,27 +3,14 @@
 //TODO: Check for other texture formats: ARGB BRGA Ecc FRMO PGRAPH CODE nv097 cases and convert with SDL.
 void texture_download(TextureBinding *texture, SurfaceBinding *surface)
 {
-     SDL_surface * surf;
-     char filename[20];
+    SDL_surface * surf;
+    char filename[20];
 
     snprintf(filename, sizeof(filename), "%lu.bmp", surface->vram_addr);
-   
-   
-   switch(surface->shape.color_format) {
-        case NV097_SET_TEXTURE_FORMAT_COLOR_SZ_Y8:
-        surf = SDL_CreateY
-
-
-
-
-
-
-       case NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R8G8B8A8:
-       surf = SDL_CreateRGBSurfaceFrom(surface->texture, surface->width, surface->height, 8*4, surface->width*4, 0,0,0,0);
-       SDL_SaveBMP(surf, filename);
-       break;
-       
-   }
+    surf = SDL_CreateRGBSurfaceFrom(surface->texture, surface->width, surface->height, 
+                                    surface->shape.color_format*surface->fmt.bytes_per_pixel, surface->width*4, 0,0,0,0);
+    SDL_SaveBMP(surf, filename);   
+}
    
 
    
